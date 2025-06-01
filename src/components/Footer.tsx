2849c5cbf0c1447441716d1302b847811396ffc1
Nitish Mehta <101048101+nitishkumar80@users.noreply.github.com>
2025-06-02 02:47:36 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -75,11 +75,11 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-2">
                 <Phone className="h-4 w-4" />
-                <span>+1 (555) 123-4567</span>
+                <span>+91 _____________</span>
               </div>
               <div className="flex items-center space-x-2">
                 <MapPin className="h-4 w-4" />
-                <span>Hollywood, CA 90028</span>
+                <span>-_____, _______</span>
               </div>
             </div>
           </div>
